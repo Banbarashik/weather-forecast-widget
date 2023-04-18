3f528865db5219ac8d6a6cc0627d4bfef6e2e8a9
Banbarashik <odinokyskitalec@yahoo.com>
2023-04-18 13:37:07 +0400

    create an empty 'formatCurrentDate' func

--- src/js/helper.js
+++ src/js/helper.js
@@ -1,4 +1,9 @@
+// prettier-ignore
+const weekdays = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+
 export async function fetchAndParse(url) {
-    const res = await fetch(url);
-    return res.json();
-  }
+  const res = await fetch(url);
+  return res.json();
+}
+
+export function formatCurrentDate(date) {}

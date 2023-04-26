72e14f925c754a9524cf347af85abd5a4cc39d24
Banbarashik <odinokyskitalec@yahoo.com>
2023-04-26 13:20:27 +0400

    create 'getHourIn24hrFormat' and 'getHourIn12hrFormat' helper functions

--- src/js/helper.js
+++ src/js/helper.js
@@ -11,6 +11,12 @@ const isToday = date => date.toDateString() === new Date().toDateString();
 export const getDayName = date =>
   isToday(date) ? 'Today' : weekdays[date.getDay()].slice(0, 3);
 
+export const getHourIn24hrFormat = date =>
+  date.getHours() + ':' + date.getMinutes();
+
+export const getHourIn12hrFormat = date =>
+  date.getHours() + ':' + date.getMinutes() + 'PM';
+
 export function formatDate(date) {
   const dayName = weekdays[date.getDay()];
   const time = date.getHours() + ':' + date.getMinutes();
